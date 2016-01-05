06bb6e925008793bb6734b8ff2c0879fd50dacb1
Alison Monteiro <alisonmonteiro.10@gmail.com>
2016-01-04 21:16:20 -0300

    Starting with ECMAScrit2015

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
-var fs = require('fs'),
+'use strict';
+
+let fs = require('fs'),
     observed = require('observed'),
     steno = require('steno'),
     path = require('path'),
     mkdirp = require('mkdirp'),
+    helpers = require('./lib/helpers');
+
+let keepExtension = helpers.keepExtension,
+    existsSync = helpers.existsSync,
     tmp = {};
 
-var h = require('./lib/helpers'),
-    keepExtension = h.keepExtension,
-    existsSync = h.existsSync;
+let write = function(file, parsed){
+  // steno is used to prevent race condition
+  steno.writeFile(file, JSON.stringify(parsed, null, '\t'));
+}
 
-var bjson = function(file, cb){
-    file = keepExtension(file, '.json');
-    if(tmp[file]) {
-        return tmp[file];
-    }
-    if(!existsSync(file)){
-        mkdirp.sync(path.dirname(file));
-        fs.writeFileSync(file, '{}');
-    }
-    var parsed = JSON.parse(fs.readFileSync(file));
-    var observe = observed(parsed);
-    var write = function(){
-        // steno is used to prevent race condition
-        steno.writeFile(file, JSON.stringify(parsed, null, '\t'));
-    }
-    observe.on('change', function(){
-        write();
-    });
-    if(typeof cb === "function") {
-        cb(observe);
-    }
-    return tmp[file] = parsed;
+let parseJson = (file) => {
+  return JSON.parse(fs.readFileSync(file));
+}
+
+let bjson = (file, cb) => {
+  let parsed,
+      observe;
+
+  file = keepExtension(file, '.json');
+  if(tmp[file]) {
+      return tmp[file];
+  }
+
+  if(!existsSync(file)){
+      mkdirp.sync(path.dirname(file));
+      fs.writeFileSync(file, '{}');
+  }
+
+  parsed = parseJson(file);
+  observe = observed(parsed);
+
+  observe.on('change', () => {
+    write(file, parsed);
+  });
+
+  if(typeof cb === "function") {
+    cb(observe);
+  }
+  
+  return tmp[file] = parsed;
 }
 
 module.exports = bjson;
